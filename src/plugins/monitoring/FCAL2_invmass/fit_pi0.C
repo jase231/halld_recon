fb65eea3f4612f5c87ded32011e726d9e089077a
staylorjlab <staylor@ifarm2402.jlab.org>
2026-07-21 07:36:12 -0400

    Change background function and and BCAL

--- src/plugins/monitoring/FCAL2_invmass/fit_pi0.C
+++ src/plugins/monitoring/FCAL2_invmass/fit_pi0.C
@@ -1,9 +1,9 @@
 void fit_pi0(){
-  TCanvas *c1=new TCanvas();
-  c1->Divide(2,1);
+  TCanvas *c1=new TCanvas("c1","Two photon mass distributions",1920,960);
+  c1->Divide(3,1);
   c1->cd(1);
-  TF1 *f1=new TF1("f1","gaus(0)+pol2(3)",0.09,0.17);
-  f1->SetParameters(100,0.125,0.004,0,0,0);
+  TF1 *f1=new TF1("f1","gaus(0)+expo(3)",0.09,0.17);
+  f1->SetParameters(100,0.125,0.004,2,-10,0);
   TH1F *h1=(TH1F*)_file0->FindObjectAny("h_2gamma_ECAL_ECAL");
   h1->GetXaxis()->SetRangeUser(0.08,0.18);
   f1->SetParameter(0,h1->GetMaximum());
@@ -11,5 +11,19 @@ void fit_pi0(){
   c1->cd(2);
   TH1F *h2=(TH1F*)_file0->FindObjectAny("h_2gamma_FCAL_FCAL");
   h2->GetXaxis()->SetRangeUser(0.08,0.18);
+  f1->SetParameter(3,2);
+  f1->SetParameter(4,-10);
+  f1->SetParameter(2,0.008);
+  f1->SetParameter(1,0.135);
+  f1->SetParameter(0,h2->GetMaximum());
   h2->Fit("f1");
+  c1->cd(3);
+  TH1F *h3=(TH1F*)_file0->FindObjectAny("h_2gamma_BCAL_BCAL");
+  f1->SetParameter(1,0.135);
+  f1->SetParameter(2,0.01);
+  f1->SetParameter(3,2);
+  f1->SetParameter(4,-10);
+  f1->SetParameter(0,h3->GetMaximum());
+  h3->GetXaxis()->SetRangeUser(0.08,0.18);
+  h3->Fit("f1");
 }
